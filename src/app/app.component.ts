20b89d0b7679a22cb22b1e9cbffff60beee81965
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 15:21:04 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  highligtColor = 'white';
 
-
+  highligt(color: string) {
+    this.highligtColor = color;
+  }
 }
