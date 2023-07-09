3c448b166cd7f56d59837f09ff5728f79dec0a92
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 15:50:49 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // ngClass
   highligtColor = 'white';
 
   highligt(color: string) {
     this.highligtColor = color;
+  }
+
+  // ngStyle
+  isStyleApplied = false;
+  toggleStyle() {
+    this.isStyleApplied = !this.isStyleApplied;
   }
 }
